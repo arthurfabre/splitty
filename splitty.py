5c71289f6cdea228c4a330fe638d2106a53f441d
Arthur Fabre <arthur@arthurfabre.com>
2018-06-02 09:49:48 +0200

    Replace float with decimal, add precision control
    Switch from representing money amounts as floats to decimals, this
    avoids the pains of floating point innacuracies (eg all the balances not
    adding up to exactly 0).
    
    Add a precision flag, which determines the lowest usable amount of
    currency when splitting an expense.

--- splitty.py
+++ splitty.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import decimal
 import csv
 import sys
 import argparse
@@ -10,7 +11,7 @@ import argparse
 class Person:
     def __init__(self, name):
         self.name = name
-        self.balance = 0.0
+        self.balance = decimal.Decimal(0)
 
     def __repr__(self):
         return "<Person name:%s balance:%f>" % (self.name, self.balance)
@@ -22,12 +23,22 @@ class Expense:
         self.splitters = splitters
 
     # Update the balance of all persons involved
-    def balance(self):
-        split_amount = self.amount / len(self.splitters)
+    def balance(self, precision):
+        # Scale things so we can use integer division - ensures the sum of the splits is the original amount
+        factor = 1 / precision
+        split_amount = ((self.amount * factor) // len(self.splitters)) / factor
+        split_remain = ((self.amount * factor) % len(self.splitters)) / factor
 
         for splitter in self.splitters:
-            splitter.balance -= split_amount
-            self.payer.balance += split_amount
+            splitter_amount = split_amount
+
+            # Use up the remainder as we go along
+            if split_remain > 0:
+                splitter_amount += precision
+                split_remain -= precision
+
+            splitter.balance -= splitter_amount
+            self.payer.balance += splitter_amount
 
 class Transaction:
     def __init__(self, amount, sender, recipient):
@@ -35,7 +46,7 @@ class Transaction:
         self.sender = sender
         self.recipient = recipient
 
-def parse_csv(expenses_file, transactions_file):
+def parse_csv(expenses_file):
     expenses = []
     persons = {}
 
@@ -43,17 +54,17 @@ def parse_csv(expenses_file, transactions_file):
     expenses_reader = csv.DictReader(expenses_file, fieldnames=['payer', 'amount', 'splitters'])
     for row in expenses_reader:
         payer = persons.setdefault(row['payer'], Person(row['payer']))
-        amount = float(row['amount'])
+        amount = decimal.Decimal(row['amount'])
         splitters = [persons.setdefault(name, Person(name)) for name in row['splitters'].split()]
 
         expenses.append(Expense(amount, payer, splitters))
 
     return expenses, persons
 
-def transactions(expenses, persons):
+def transactions(expenses, persons, precision):
     # Balance all the expenses to get everyone's final balance
     for expense in expenses:
-        expense.balance()
+        expense.balance(precision)
 
     # Check everyone's balance adds up to 0
     print(persons)
@@ -67,6 +78,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Split some expenses into a minimal set of transactions.')
     parser.add_argument('expenses', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help="CSV formatted list of expenses to read")
     parser.add_argument('transactions', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help="CSV formatted list of minimal transactions to write")
+    parser.add_argument('-p', '--precision', type=decimal.Decimal, default=decimal.Decimal('0.01'), help="Smallest usable currency amount when splitting expenses")
     args = parser.parse_args()
 
-    transactions(*parse_csv(args.expenses, args.transactions))
+    transactions(*parse_csv(args.expenses), args.precision)
